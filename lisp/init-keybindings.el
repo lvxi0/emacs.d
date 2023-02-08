a4ba15d76cec6e0c4e35532701caf1e0b9681329
lxgcc <lvxi0369@gmail.com>
2023-02-08 15:58:25 +0800

    add f

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -15,4 +15,8 @@
 
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
+(global-set-key (kbd "C-c p f") 'counsel-git)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
 (provide 'init-keybindings)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,6 +2,6 @@
 (require 'org-tempo)
 (setq org-src-fontify-natively t)
 (setq org-agenda-files '("~/Work/org"))
-(global-set-key (kbd "C-c a") 'org-agenda)
+
 
 (provide 'init-org)
